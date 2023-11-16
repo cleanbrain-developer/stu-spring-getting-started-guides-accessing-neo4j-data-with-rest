e0bfde3d40bc94034a7d5ef187f9b1290522fabf
cleanbrain.developer <cleanbrain.developer@gmail.com>
2023-11-16 21:10:31 +0900

    first commit

---
+++ src/main/java/com/example/StuSpringGettingStartedGuidesAccessingNeo4jDataWithRestApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StuSpringGettingStartedGuidesAccessingNeo4jDataWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StuSpringGettingStartedGuidesAccessingNeo4jDataWithRestApplication.class, args);
+	}
+
+}
